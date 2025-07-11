dfc070d77d56c70f9c37e8a512a0d8e4b1b80f63
Lucy <lucywang340@gmail.com>
2025-07-11 09:55:58 -0500

    Initial Commit

---
+++ birthday2/birthday2App.swift
@@ -0,0 +1,17 @@
+//
+//  birthday2App.swift
+//  birthday2
+//
+//  Created by Scholar on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct birthday2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
